5da67bb1f12ec649873ac0a466d1700decd5248d
KoyoteX <caue8721@gmail.com>
2026-09-16 08:29:18 -0300

    Update teste.py
    salvei novamente

--- teste.py
+++ teste.py
@@ -2,6 +2,6 @@ a = int(input("Digite o valor de A: "))
 
 b = int(input("Digite o valor de B: "))
 
-c = a + b
+c = a * b
 
-print(f"A soma de A e B é: {c}")
+print(f"A multiplicação de A e B é: {c}")
